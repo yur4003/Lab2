2dc4b82bd6b1a072d9e06612e21d026461360196
Yuxiang R <yur4003@med.cornell.edu>
2023-10-29 04:48:34 -0400

    Set the doc frames of Function 1 & 2 Delated hello.R

--- R/function1.R
+++ R/function1.R
@@ -1,1 +1,18 @@
 # Function1
+#' function1
+#' Make a boxplot of payments by DRG code
+#' Make it an option for your function to do this for either the average Medicare payments,
+#' the average total payment, or the average covered charges
+#'
+#' @param
+#' @return A boxplot of payments by DRG code
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 geom_boxplot
+#' @export
+#'
+#' @examples
+#'
+#'
+func1 <- function(){
+
+}
--- R/function2.R
+++ R/function2.R
@@ -1,1 +1,14 @@
-# function2
+
+#' Function 2
+#'
+#' @return
+#' @export
+#' Calculates statistics over all of the DRG codes for average Medicare payments.
+#' Make it an option  to calculate either the mean, median,
+#' or standard deviation of the DRG codes.
+#' @param
+#' @importFrom
+#' @examples
+function2 <-function(){
+
+}
