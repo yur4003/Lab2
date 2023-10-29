7e54ca12fbdbadb965a5b2d2a71fe22f8ccdb567
Yuxiang R <yur4003@med.cornell.edu>
2023-10-29 15:34:44 -0400

    update data variable names ; File name changed

--- R/function_2.R
+++ R/function_2.R
@@ -1,30 +1,33 @@
 
-#' Function 2
-#'
-#' @return
+#' Function_2
 #' @export
 #' Calculates statistics over all of the DRG codes for average Medicare payments.
 #' Make it an option  to calculate either the mean, median,
 #' or standard deviation of the DRG codes.
-#' @param
+#' @param x a string from c("mean","median","standard deviation")
 #' @importFrom
 #' @examples
-# Function 2: Write a function that calculates statistics over all of the DRG codes for average Medicare payments.
-# Make it an option in your function to calculate either the mean, median, or standard deviation of the DRG codes.
+
 library(tidyverse)
 library(ggplot2)
 
-DRG_data <- read_csv("DRG_data.csv")
+load("~/Desktop/wcm_term1/DS1/Lab2/data/DRG_data.RData")
 
-function2 <- function(x){
-  if(x == "mean"){
-    mean <- round(mean(DRG_data$`Average Medicare Payments`), 2)
+function_2<-function(x){
+  if(x=="mean"){
+    mean <- round(mean(DRG_data$Average.Medicare.Payments), 2)
     print(paste("The mean of average Medicare payments is", mean))
   } else if(x == "median"){
-    median <- round(median(DRG_data$`Average Medicare Payments`), 2)
+    median <- round(median(DRG_data$Average.Medicare.Payments), 2)
     print(paste("The median of average Medicare payments is", median))
   } else if(x == "standard deviation"){
-    sd <- round(sd(DRG_data$`Average Medicare Payments`), 2)
+    sd <- round(sd(DRG_data$Average.Medicare.Payments), 2)
     print(paste("The standard deviation of average Medicare payments is", sd))
   } else print("Invalid input, you must choose from mean, median and standard deviation ")
 }
+
+
+
+
+
+
